276bdb884a16e293a8fb32605bbcbf9402c0ecb9
Zain Ali <zain.ali@square63.com>
2016-06-28 11:30:37 +0500

    User: Added column full_name to the User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160627071028) do
+ActiveRecord::Schema.define(version: 20160628052304) do
+
+  create_table "attachments", force: :cascade do |t|
+    t.integer  "attachable_id",      limit: 4
+    t.string   "attachable_type",    limit: 255
+    t.string   "image_file_name",    limit: 255
+    t.string   "image_content_type", limit: 255
+    t.integer  "image_file_size",    limit: 4
+    t.datetime "image_updated_at"
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+  end
+
+  add_index "attachments", ["attachable_type", "attachable_id"], name: "index_attachments_on_attachable_type_and_attachable_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
@@ -30,6 +43,7 @@ ActiveRecord::Schema.define(version: 20160627071028) do
     t.string   "unconfirmed_email",      limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.string   "full_name",              limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
