eeee100801d1fa047e2e8278f17dfd9a2fdd9624
Zain Ali <zain.ali@square63.com>
2016-06-29 15:11:59 +0500

    Adding values in actors table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Actor.create(name: 'Tom Cruise', biography: "Born in Chicago", gender: "Male")
+Actor.create(name: 'Emma Watson', biography: "Born in Australia", gender: "Female")
+Actor.create(name: 'Bruce Lee', biography: "Born in Japan", gender: "Male")
+Actor.create(name: 'Peter Parker', biography: "Born in America", gender: "Male")
