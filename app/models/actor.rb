836362763bcdf938b6a7e385d1e688b73981484b
Zain Ali <zain.ali@square63.com>
2016-07-01 10:54:10 +0500

    Added Validations in Actor and Movie model

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,4 +1,5 @@
 class Actor < ActiveRecord::Base
+  validates :name, presence: true, uniqueness: true, length: { maximum: 20 }
   has_many :casts, dependent: :destroy
   has_many :movies, through: :casts
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,7 @@
 class Movie < ActiveRecord::Base
+  validates :title, presence: true, uniqueness: true, length: { maximum: 150 }
+  validates :trailer, presence: true
+  validates :description, presence: true
   has_many :posters, class_name: "Attachment", as: :attachable, dependent: :destroy
   accepts_nested_attributes_for :posters, allow_destroy: true
   has_many :casts , dependent: :destroy
