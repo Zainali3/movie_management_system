26d0e144dca9a95a4ed49cff4f9424104f8f1290
Zain Ali <zain.ali@square63.com>
2016-07-18 14:40:48 +0500

    Added admin css files and added a field in admin movie model

--- app/admin/movie.rb
+++ app/admin/movie.rb
@@ -13,13 +13,14 @@ ActiveAdmin.register Movie do
 #   permitted
 # end
 
-  permit_params :title, :description, :trailer, :featured, :approved, posters_attributes: [:id, :image, :_destroy], actor_ids: []
+  permit_params :title, :description, :trailer, :release_date, :duration, :genre, :featured, :approved, posters_attributes: [:id, :image, :_destroy], actor_ids: []
 
   index do
     column :title
     column :description
     column :featured
     column :approved
+    column :release_date
     column :actors do |movie|
       movie.movie_cast
     end
@@ -37,6 +38,8 @@ ActiveAdmin.register Movie do
       f.input :featured
       f.input :approved
       f.input :duration
+      f.input :genre
+      f.input :release_date, as: :datepicker
       f.input :actors
       f.has_many :posters, heading: 'Posters', new_record: 'Add Poster' do |attachment|
        attachment.input :image, hint: attachment.template.image_tag(attachment.object.image.url(:thumb)), allow_destroy: true
