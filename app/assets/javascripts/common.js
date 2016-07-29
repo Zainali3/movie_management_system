7404835b5e552abc992bc6960c94138b033aac30
zain ali <zain.ali@square63.com>
2016-07-29 09:10:44 +0500

    Changed javascript function to avoid special characters in search

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -1,16 +1,12 @@
-function letternumber(e) {
-  var key;
-  var keychar;
-  if (window.event) key = window.event.keyCode;
-    else if (e) key = e.which;
-  else
-    return true;
-  keychar = String.fromCharCode(key);
-  keychar = keychar.toLowerCase();
-  if ((key==null) || (key==0) || (key==8) || (key==9) || (key==13) || (key==27) )
-    return true;
-  else if ((("abcdefghijklmnopqrstuvwxyz0123456789").indexOf(keychar) > -1))
-    return true;
-  else
-    return false;
-}
+$(document).on('ready page:load', function() {
+$('.txt-box').keypress(function (e) {
+  if (e.charCode != 0) {
+    var regex = new RegExp("^[a-zA-Z0-9\\-\\s]+$");
+    var key = String.fromCharCode(!e.charCode ? e.which : e.charCode);
+    if (!regex.test(key)) {
+      e.preventDefault();
+      return false;
+    }
+  }
+});
+});
